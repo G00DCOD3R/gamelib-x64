ad419c923a94db956444185cde5decaa6c4039ad
G00DCOD3R <82707639+G00DCOD3R@users.noreply.github.com>
2022-11-03 18:36:01 +0100

    Add files via upload

---
+++ game.s
@@ -0,0 +1,228 @@
+/*
+This file is part of gamelib-x64.
+
+Copyright (C) 2014 Tim Hegeman
+
+gamelib-x64 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gamelib-x64 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gamelib-x64. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+.file "src/game/game.s"
+
+.global gameInit
+.global gameLoop
+
+.section .game.data
+
+.keysPressed: .byte 0
+gameMode: .byte 0
+.printingDelay: .byte 0x0
+.printingTicks: .byte 0x15
+.delayTicks: .byte 0x8
+.spaceDelay: .byte 0
+.UPDelay: .byte 0
+.DOWNDelay: .byte 0 
+.LEFTDelay: .byte 0 
+.RIGHTDelay: .byte 0 
+  
+.section .game.text
+
+gameInit:
+
+	// push %r12
+	// sub $8, %rsp 
+	// mov $8, %r12 
+	// 1: 
+	// cmp $0, %r12 
+	// je 2f
+	// 	mov $10, %rdi 
+	// 	call random 
+	// 	mov %rax, %rdx 
+	// 	mov %r12, %rdi 
+	// 	mov %r12, %rsi 
+	// 	call print_number
+
+	// dec %r12 
+	// jmp 1b
+	// 2:
+	
+	// add $8, %rsp 
+	// pop %r12 
+
+	ret
+
+
+
+gameLoop:
+
+	movb $0, .keysPressed		# keep track of every key that was pressed
+
+	call .decrementDelays		# decrement all delays that are nonzero
+
+	mov $0x1C, %rdi  			# check enter
+	call queryKeyCode 
+	movb .keysPressed, %cl 
+	or %rcx, %rax
+	movb %al, .keysPressed
+	cmpb $0, .keysPressed
+	jne .somethingPressed
+
+	mov  $0x39, %rdi 			# check space (0x39)
+	call queryKeyCode 
+	shl $1, %rax
+	movb .spaceDelay, %dl  
+	cmpb $0, %dl
+	jne 2f						# clicked once every .spaceDelay
+	cmp $0, %rax 				
+	je 2f						# .spaceDelay == 0 && rax == 0 --> jump
+	movb .delayTicks, %dl 
+	movb %dl, .spaceDelay		# reset counter
+	movb .keysPressed, %cl 
+	or %rcx, %rax 
+	movb %al, .keysPressed
+	jmp .somethingPressed
+	2:
+
+
+	mov  $0x48, %rdi 			# check UP (0x48)
+	call queryKeyCode 
+	shl $3, %rax
+	movb .UPDelay, %dl  
+	cmpb $0, %dl
+	jne 2f						# clicked once every .UPDelay
+	cmp $0, %rax 				
+	je 2f						# .UPDelay == 0 && rax == 0 --> jump
+	movb .delayTicks, %dl 
+	movb %dl, .UPDelay		# reset counter
+	movb .keysPressed, %cl 
+	or %rcx, %rax 
+	movb %al, .keysPressed
+	jmp .somethingPressed
+	2:
+	
+	mov  $0x4B, %rdi 			# check LEFT (0x4B)
+	call queryKeyCode 
+	shl $2, %rax
+	movb .LEFTDelay, %dl  
+	cmpb $0, %dl
+	jne 2f						# clicked once every .LEFTDelay
+	cmp $0, %rax 				
+	je 2f						# .LEFTDelay == 0 && rax == 0 --> jump
+	movb .delayTicks, %dl 
+	movb %dl, .LEFTDelay		# reset counter
+	movb .keysPressed, %cl 
+	or %rcx, %rax 
+	movb %al, .keysPressed
+	jmp .somethingPressed
+	2:
+
+	mov  $0x4C, %rdi 			# check DOWN (0x4B)
+	call queryKeyCode 
+	shl $5, %rax
+	movb .DOWNDelay, %dl  
+	cmpb $0, %dl
+	jne 2f						# clicked once every .DOWNDelay
+	cmp $0, %rax 				
+	je 2f						# .DOWNDelay == 0 && rax == 0 --> jump
+	movb .delayTicks, %dl 
+	movb %dl, .DOWNDelay		# reset counter
+	movb .keysPressed, %cl 
+	or %rcx, %rax 
+	movb %al, .keysPressed
+	jmp .somethingPressed
+	2:
+
+
+	mov  $0x4D, %rdi 			# check RIGHT (0x4B)
+	call queryKeyCode 
+	shl $4, %rax
+	movb .RIGHTDelay, %dl  
+	cmpb $0, %dl
+	jne 2f						# clicked once every .RIGHTDelay
+	cmp $0, %rax 				
+	je 2f						# .RIGHTDelay == 0 && rax == 0 --> jump
+	movb .delayTicks, %dl 
+	movb %dl, .RIGHTDelay		# reset counter
+	movb .keysPressed, %cl 
+	or %rcx, %rax 
+	movb %al, .keysPressed
+	jmp .somethingPressed
+	2:
+
+
+	cmpb $0, gameMode 
+	jne .nothingPressed
+	.somethingPressed:
+
+	cmpb $0, gameMode 
+	jne 1f 
+	movb .keysPressed, %dil 
+	call ss_showWelcome
+	ret							# we have nothing to update, since we are in menu mode, thus returning now
+
+	1:
+	// # we are in game mode 
+	
+	movb .keysPressed, %dil 
+	call updateDirection		# one of arrow keys was pressed, so update pacman's direction
+
+	// mov $50, %rdi 
+	// mov $50, %rsi 
+	// mov dir_pacman, %rdx 
+	// call print_number
+
+	.nothingPressed:
+
+	cmpb $0, .printingDelay
+	jne 1f				# don't jump == we have to update board 
+
+	call move_characters
+	movb .printingTicks, %al 
+	movb %al, .printingDelay		# reset counter 
+	jmp 2f
+
+	1:					# we don't have to update our board yet
+	movb .printingDelay, %al 
+	dec %al 
+	movb %al, .printingDelay
+	2:
+
+
+	ret
+
+
+
+
+.decrementDelays: 
+	mov $5, %rcx 
+	mov $.spaceDelay, %rax 
+
+	1: 
+	cmp $0, %rcx 
+	je 2f
+
+		cmpb $0, (%rax)
+		je 3f
+		movb (%rax), %dl 
+		dec %dl 
+		movb %dl, (%rax)
+		3:
+
+	inc %rax 
+	dec %rcx
+	jmp 1b 
+	2:
+
+	ret
+
+
---
+++ panel.s
@@ -0,0 +1,192 @@
+.file "src/game/panel.s"
+
+.section .game.data
+
+score:
+    .byte 0
+high_score:
+    .byte 0
+digits:
+    .skip 3
+
+caption:
+    .asciz "Tymon and Radoslaw's"
+    .asciz "game PAC-MAN"
+
+score_text:
+    .asciz "SCORE:"
+    .asciz "HIGH SCORE:"
+
+.section .game.text
+
+# Subroutine prints captions that are constantly on panel during the game
+setup_panel:
+    push    %rbp
+    mov     %rsp, %rbp
+
+    movq    $50, %rdi
+    movq    $0, %rsi
+1:  movq    $0, %rcx
+    movq    $0, %rdx
+
+    pushq   %rdi
+    pushq   %rsi
+    call    putChar
+    popq    %rsi
+    popq    %rdi
+
+    incq    %rdi
+    cmpq    $80, %rdi
+    jne     1b
+
+    movq    $50, %rdi
+    incq    %rsi
+    cmpq    $25, %rsi
+    jne     1b
+
+    movq    $55, %rdi
+    movq    $5, %rsi
+    movq    $caption, %rax
+1:  movb    (%rax), %dl
+    movb    $0x0e, %cl
+
+    pushq   %rdi
+    pushq   %rsi
+    pushq   %rax
+    subq    $8, %rsp
+    call    putChar
+    addq    $8, %rsp
+    popq    %rax
+    popq    %rsi
+    popq    %rdi
+
+    incq    %rdi
+    incq    %rax
+    cmpb    $0, (%rax)
+    jne     1b
+
+    incq    %rax
+    movq    $59, %rdi
+    incq    %rsi
+    cmpq    $7, %rsi
+    jne     1b
+
+    movq    $52, %rdi
+    movq    $9, %rsi
+    movq    $score_text, %rax
+1:  movb    (%rax), %dl
+    movb    $0x0e, %cl
+
+    pushq   %rdi
+    pushq   %rsi
+    pushq   %rax
+    subq    $8, %rsp
+    call    putChar
+    addq    $8, %rsp
+    popq    %rax
+    popq    %rsi
+    popq    %rdi
+
+    incq    %rdi
+    incq    %rax
+    cmpb    $0, (%rax)
+    jne     1b
+
+    movq    $52, %rdi
+    incq    %rsi
+    incq    %rax
+    cmpq    $11, %rsi
+    jne     1b
+
+    movq    $58, %rdi
+    movq    $9, %rsi
+    movq    $0, %rdx
+    call    print_number
+
+    movq    $63, %rdi
+    movq    $10, %rsi
+    movq    $0, %rdx
+    movb    high_score, %dl
+    call    print_number
+
+    mov     %rbp, %rsp
+    pop     %rbp
+
+    ret
+
+# print_number(int x, int y, int num)
+# Subroutine prints a given number starting from given cell.
+# This does not work if a number is greater that 999!
+print_number:
+    push    %rbp
+    mov     %rsp, %rbp
+    pushq   %rsi
+    pushq   %rdi
+    movq    %rdx, %rax
+
+    cmpq    $0, %rax
+    je      case_0
+
+    movq    $digits, %rcx
+    movb    $0, (%rcx)
+    movb    $0, 1(%rcx)
+    movb    $0, 2(%rcx)
+
+1:  cmpq    $100, %rax
+    jl      1f
+    subq    $100, %rax
+    incq    (%rcx)
+    jmp     1b
+
+1:  incq    %rcx
+1:  cmpq    $10, %rax
+    jl      1f
+    subq    $10, %rax
+    incq    (%rcx)
+    jmp     1b
+
+1:  incq    %rcx
+    addq    %rax, (%rcx)
+
+    movq    $digits, %rax
+1:  cmpb    $0, (%rax)
+    jne     1f
+    incq    %rax
+    jmp     1b
+
+1:  movq    (%rsp), %rdi
+1:  movq    $48, %rdx
+    addb    (%rax), %dl
+    movq    8(%rsp), %rsi
+    movq    $0x0e, %rcx
+    pushq   %rax
+    pushq   %rdi
+    call    putChar
+    popq    %rdi
+    popq    %rax
+    incq    %rdi
+    incq    %rax
+    movq    $digits, %r8
+    addq    $3, %r8
+    cmpq    %r8, %rax
+    jne     1b
+
+    addq    $16, %rsp
+
+    mov     %rbp, %rsp
+    pop     %rbp
+
+    ret
+
+case_0:
+    movq    (%rsp), %rdi
+    movq    8(%rsp), %rsi
+    movq    $0x0e, %rcx
+    movq    $48, %rdx
+    call    putChar
+    addq    $16, %rsp
+
+    mov     %rbp, %rsp
+    pop     %rbp
+
+    ret
